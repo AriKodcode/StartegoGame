bec0fd400b827c2103450dcd0743ddc50bf0a317
Ari Durlacher <arikodcode@gmail.com>
2025-12-08 11:29:51 +0200

    fixed createSoldier function

--- utils/createSoldire.js
+++ utils/createSoldire.js
@@ -1,10 +1,6 @@
 export default function createSoldier(rank, belong) {
-  let rankOfSoldier = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   let move = true;
-  if (rankOfSoldier.includes(rank)) {
-    rank = Number(rank);
-  }
-  if (rank === "boom" || rank === "flag") {
+  if (rank === 'boom' || rank === 'flag') {
     move = false;
   }
   let soldier = {
