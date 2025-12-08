4cc14d725ae331c9e0a706fa62ea0e98439d98da
Ari Durlacher <arikodcode@gmail.com>
2025-12-08 11:24:21 +0200

    create createArmy function

---
+++ utils/createArmy.js
@@ -0,0 +1,25 @@
+import createSoldier from './createSoldire.js';
+
+export default function cretaeArmy(player) {
+  let soldiers = [];
+  const soldiersObj = [
+    { rank: 1, amout: 1 },
+    { rank: 2, amout: 8 },
+    { rank: 'boom', amout: 5 },
+    { rank: 4, amout: 4 },
+    { rank: 5, amout: 4 },
+    { rank: 6, amout: 4 },
+    { rank: 7, amout: 3 },
+    { rank: 8, amout: 2 },
+    { rank: 9, amout: 1 },
+    { rank: 10, amout: 1 },
+    { rank: 'flag', amout: 1 },
+  ];
+  for (let i = 0; i < soldiersObj.length; i++) {
+    for (let j = 0; j < soldiersObj[i]['amout']; j++) {
+      let soldier = createSoldier(soldiersObj[i]['rank'], player);
+      soldiers.push(soldier);
+    }
+  }
+  return soldiers;
+}
