70ea8b0b773a21fe97f3f1fa59b4cff10c00c461
Ari Durlacher <arikodcode@gmail.com>
2025-12-09 15:42:25 +0200

    create playerPlayRound function and fix showBord function and fixed whoIsWinnig function

--- gameLogic/whoIsWinnig.js
+++ gameLogic/whoIsWinnig.js
@@ -1,15 +1,17 @@
 export default function whoIsWinning(attacker, defender) {
-  if (attacker["rank"] === 3 && defender["rank"] === "boom") {
-    return "attacker win";
-  } else if (attacker["rank"] === 1 && defender["rank"] === 10) {
-    return "attacker win";
-  } else if (attacker["rank"] > defender["rank"]) {
-    return "attacker win";
-  } else if (attacker["rank"] < defender["rank"]) {
+  if (attacker['rank'] === 3 && defender['rank'] === 'boom') {
+    return 'attacker win';
+  } else if (attacker['rank'] !== 3 && defender['rank'] === 'boom') {
     return `defender win`;
-  } else if (attacker["rank"] === defender["rank"]) {
-    return "draw";
-  } else if (defender["rank"] === "flag") {
+  } else if (attacker['rank'] === 1 && defender['rank'] === 10) {
+    return 'attacker win';
+  } else if (attacker['rank'] > defender['rank']) {
+    return 'attacker win';
+  } else if (attacker['rank'] < defender['rank']) {
+    return `defender win`;
+  } else if (attacker['rank'] === defender['rank']) {
+    return 'draw';
+  } else if (defender['rank'] === 'flag') {
     return `${attacker[`belongTo`]} win the game`;
   }
 }
